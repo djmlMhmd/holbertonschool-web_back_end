f34e444ee616e90d420c1f13f9bf5b5832aa4a3a
djamalMhmd <djamaldine-mohamed@live.fr>
2023-08-29 10:52:12 +0200

    remove white space

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ async def wait_random(max_delay: int = 10) -> float:
     Asynchronous routine that spawns wait_random
     n times with specified max_delay.
     """
-    
+
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
