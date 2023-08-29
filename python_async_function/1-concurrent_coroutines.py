aaad320c930212d097f768a37961d779bc8948d5
djamalMhmd <djamaldine-mohamed@live.fr>
2023-08-29 10:48:37 +0200

    update

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""function with integers n and max_delay as
+"""function with integers n and max_delay as 
 arguments that measures the total execution"""
 
 import asyncio
@@ -8,13 +8,9 @@ from typing import List
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous routine that spawns wait_random n times with specified max_delay.
+    Asynchronous routine that spawns wait_random
+    n times with specified max_delay.
 
     Args:
         n (int): Number of times to spawn wait_random.
@@ -23,6 +19,13 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: List of delays in ascending order.
     """
+        
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+
     tasks = [wait_random(max_delay) for _ in range(n)]
 
     delays = await asyncio.gather(*tasks)
