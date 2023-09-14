cefcf0ab60200ab1fe060016d0a3a9f54e72b034
djamalMhmd <djamaldine-mohamed@live.fr>
2023-09-14 20:15:23 +0200

    add export

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,6 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const sizeRooms = [19, 20, 34];
-  const rooms = sizeRooms.map((size) => new ClassRoom(size));
+export default function initializeRooms() {
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
   return rooms;
 }
