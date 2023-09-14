6be13ec1050a05b85a87ec2654147516fc7f63c2
djamalMhmd <djamaldine-mohamed@live.fr>
2023-09-14 20:12:24 +0200

    update

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  const size_rooms = [19, 20, 24];
-  const rooms = size_rooms.map((size) => new ClassRoom(size));
+  const sizeRooms = [19, 20, 34];
+  const rooms = sizeRooms.map((size) => new ClassRoom(size));
   return rooms;
 }
