e1cfd25114d49922fed76f087050b76a4e583d53
djamalMhmd <djamaldine-mohamed@live.fr>
2023-09-14 21:06:38 +0200

    Implement a class named HolbertonCourse

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolberCourse {
+	constructor(name, length, students) {
+		this.name = name;
+		this.length = length;
+		this.students = students;
+	}
+	get name() {
+		return this._name;
+	}
+
+	get length() {
+		return this._length;
+	}
+	get students() {
+		return this._students;
+	}
+	set name(newName) {
+		if (typeof newName !== 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		this._name = newName;
+	}
+	set length(newLength) {
+		if (typeof newLength !== 'number') {
+			throw new TypeError('Length must be a number');
+		}
+		this._length = newLength;
+	}
+	set students(newStudents) {
+		if (Array.isArray(newStudents)) {
+			for (let student of newStudents) {
+				if (typeof student !== 'string') {
+					throw new TypeError('Students must be an array of strings');
+				}
+			}
+		}
+		this._students = newStudents;
+	}
+}
