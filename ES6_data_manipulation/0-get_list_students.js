caf40cc110d5b9a1c85682a44183267dc8e7d24f
djamalMhmd <djamaldine-mohamed@live.fr>
2023-10-10 15:35:19 +0200

    change function

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
+// 0-get_list_students.js
+
 function getListStudents() {
-  return [
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-}
 
-const students = getListStudents();
+  return students;
+}
